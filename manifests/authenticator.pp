26f24fed6a21139c1887454f54fa81b4ae43f56b
Mesar Hameed <m.m.hameed@bath.ac.uk>
2016-08-16 14:10:10 +0100

    authenticators can be used when acting as a smarthost client.

--- manifests/authenticator.pp
+++ manifests/authenticator.pp
@@ -19,6 +19,14 @@
 # [*server_prompts*]
 #  Promt used in the smtp session to ask for data  (User: Password:)
 #
+# [*client_ignore_invalid_base64*]
+#  If the client receives a server prompt that is not a valid base64 string, authentication is abandoned by 
+#  default. However, if this option is set true, the error in the challenge is ignored and the client 
+#  sends the response as usual. 
+#
+# [*client_send*]
+#  Set the string to be sent when acting as a client, i.e. smarthost.
+#
 
 define exim::authenticator (
   $driver,
@@ -26,7 +34,12 @@ define exim::authenticator (
   $server_condition,
   $server_set_id,
   $server_prompts = undef,
+  $client_ignore_invalid_base64 = false,
+  $client_send    = undef,
   ){
+
+  if ($client_ignore_invalid_base64 ) { validate_bool($client_ignore_invalid_base64 ) }
+  if ($client_send) { validate_array($client_send) }
   concat::fragment { "authenticator-${title}":
     target  => $::exim::config_path,
     content => template("${module_name}/authenticator/authenticator.erb"),
